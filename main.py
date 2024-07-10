b6d5317b873397e21e239c26c24e91c22042268d
Halaés Nobre <halaes.nobre@outlook.com>
2024-07-09 22:18:28 -0300

    :recycle: refactor: Reestrutura o arquivo main.

--- main.py
+++ main.py
@@ -1,59 +1,79 @@
-class Conta:
-    def __init__(self, agencia, conta, saldo):
-        self.agencia = agencia 
-        self.conta = conta
-        self.saldo = saldo
-        self.historico = ''
-        self.limite_saque = 500
-        self.limite_qtd_saque = 3
-    
-    def depositar(self, valor):
-        if valor > 0:
-            self.saldo += valor
-            self.historico += f'Deposito de R$ {valor:.2f}\n'
-            return ('Deposito efetuado com sucesso.')
-        else:
-            return ('Deposito inválido. Tente novamente.')    
-    
-    def sacar(self, valor):
-        if valor > self.saldo:
-            return ('Saldo insuficiente.')
-        elif valor > self.limite_saque:
-            return ('Limite de saque excedido. Tente novamente.')
-        elif self.limite_qtd_saque == 0:
-            return ('Limite de saques atingido. Tente novamente em 24h.')
-        else:
-            self.saldo -= valor
-            self.limite_qtd_saque -= 1
-            self.historico += f'Saque de R$ {valor:.2f}\n'
-            return ('Saque efetuado com sucesso.')
+from cliente import Cliente
+from conta import Conta
 
-    def mostrar_extrato(self):
-        extrato = "\n================ EXTRATO ================\n"
-        if self.historico == '':
-            extrato += 'Nenhum movimento realizado.'
-        else:
-            extrato += str(self.historico)
-        extrato += f"\nSaldo: R$ {self.saldo:.2f}"
-        extrato += "\n=========================================="
-        extrato += "\n"
-        return extrato
-
+clientes = []
 
 if __name__ == '__main__':
-    menu = 'Menu de operações:\n1 - Depositar\n2 - Sacar\n3 - Extrato\n4 - Sair\n'
-    conta = Conta(agencia=123, conta=123456, saldo=0)
+    menu = """Menu de operações:\n
+    1 - Operações de cadastro\n
+    2 - Operações de conta\n
+    0 - Sair\n"""
+    sub_menu_cadastro = """
+    1 - Cadastrar novo cliente\n
+    2 - Cadastrar conta\n
+    0 - retornar\n"""
+    sub_menu_conta = """
+    1 - Depositar\n
+    2 - Sacar\n
+    3 - Extrato\n
+    0 - retornar\n
+    """
     while True:
         print(menu)
         opcao = int(input('Escolha uma opção: '))
-
         if opcao == 1:
-            valor = float(input('Qual valor deseja depositar? '))
-            print(conta.depositar(valor))
+            cadastro = True
+            while cadastro == True:
+                print(sub_menu_cadastro)
+                opcao_cadastro = int(input('Escolha uma opção: '))
+                if opcao_cadastro == 1:
+                    nome = input('Informe o Nome completo: ')
+                    dtnasc = input('Data de nascimento: *dd-mm-aaaa* ')
+                    cpf = input('CPF: *somente numeros* ')
+                    endereco = input('Endereço: *Logradouro, nro - bairro - cidade/UF* ')
+                    cliente = Cliente(nome, dtnasc, cpf, endereco)
+                    clientes.append(cliente)
+                if opcao_cadastro == 2:
+                    cpf = input('Informe o CPF do cliente: *somente numeros* ')
+                    cliente = next((c for c in clientes if c.cpf == cpf), None)
+                    if not cliente:
+                        print('Cliente não encontrado')
+                        continue
+                    else:
+                        conta = input('Conta: ')
+                        cliente.inserir_conta(Conta(agencia="0001", conta=conta, saldo=0))
+                        print('Conta criada com sucesso!')
+                if opcao_cadastro == 0:
+                    cadastro = False
         if opcao == 2:
-            valor = float(input('Qual valor deseja sacar? '))
-            print(conta.sacar(valor))
-        if opcao == 3:
-            print(conta.mostrar_extrato())
-        if opcao == 4:
+            cpf = input('Informe o CPF do cliente: *somente numeros* ')
+            cliente = next((c for c in clientes if c.cpf == cpf), None)
+            if not cliente:
+                print('Cliente não encontrado')
+                continue
+            else:
+                conta = input('Informe a Conta: ')
+                conta = next((c for c in cliente.contas if c.conta == conta), None)
+                if not conta:
+                    print('Conta não encontrada')
+                    continue
+                else:                
+                    operacoes = True
+                    while operacoes == True:
+                        print(sub_menu_conta)
+                        opcao_operacao = int(input('Escolha uma opção: '))
+                        if opcao_operacao == 1:
+                            valor = float(input('Qual valor deseja depositar? '))
+                            print(conta.depositar(valor))
+                        if opcao_operacao == 2:
+                            valor = float(input('Qual valor deseja sacar? '))
+                            print(conta.sacar(valor))
+                        if opcao_operacao == 3:
+                            print(conta.mostrar_extrato())
+                        if opcao_operacao == 0:
+                            break
+                if opcao_operacao == 0:
+                    operacoes = False
+
+        if opcao == 0:
             break
